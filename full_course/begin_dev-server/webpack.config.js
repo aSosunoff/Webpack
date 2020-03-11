3dfbcfbb8bd6f1d31b02bff6adc204d25e7c29e8
asosunoff <al.sosunoff@gmail.com>
2020-03-11 19:46:55 +0300

    update

---
+++ full_course/begin_dev-server/webpack.config.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+
+    mode: 'development',
+
+    entry: {
+        main: './index.js',
+        analytics: './analytics.js'
+    },
+
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),   
+    },
+
+    resolve: {
+        extensions: [],
+        alias:{
+            '@model': path.resolve(__dirname, 'src/model'),
+            '@style': path.resolve(__dirname, 'src/styles'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
+
+    optimization: {
+        // noEmitOnErrors: true,
+        // runtimeChunk: {
+        //     name: 'webpack_head'//entrypoint => `runtime~${entrypoint.name}`
+        // },
+        splitChunks: {
+            chunks: 'all',
+            // minSize: 1,
+            // minChunks: 2,
+            // name: 'common',
+            // cacheGroups: {
+            //     vendor: {
+            //         test: /[\\/]node_modules[\\/]/,
+            //         name: 'vendors',
+            //         chunks: 'all'
+            //     }
+            // },
+        }
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+        
+        new CleanWebpackPlugin()
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/i,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/i,
+                use: ['file-loader']
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.csv$/i,
+                use: ['csv-loader']
+            }
+        ]
+    }
+};
